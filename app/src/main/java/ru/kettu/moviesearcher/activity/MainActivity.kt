5856266cbd5ab024df539a076a69770d981bdedc
nataliya.lebedeva <nataliya.i.lebedeva@rt.ru>
2020-01-31 00:25:05 +0300

    replace some onClick actions with onClickListeners

--- app/src/main/java/ru/kettu/moviesearcher/activity/MainActivity.kt
+++ app/src/main/java/ru/kettu/moviesearcher/activity/MainActivity.kt
@@ -1,17 +1,13 @@
 package ru.kettu.moviesearcher.activity
 
 import android.content.Intent
-import android.content.Intent.ACTION_SEND
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
@@ -20,8 +16,8 @@ import kotlinx.android.synthetic.main.item_main_header.*
 import ru.kettu.moviesearcher.R
 import ru.kettu.moviesearcher.activity.FilmDetailActivity.Companion.DETAILS_INFO
 import ru.kettu.moviesearcher.adapter.MainActivityAdapter
-import ru.kettu.moviesearcher.models.item.FavouriteItem
 import ru.kettu.moviesearcher.models.FilmDetailsInfo
+import ru.kettu.moviesearcher.models.item.FavouriteItem
 import ru.kettu.moviesearcher.models.item.FilmItem
 import ru.kettu.moviesearcher.operations.initFilmItems
 import ru.kettu.moviesearcher.operations.openFavouritesActivity
@@ -83,31 +79,10 @@ class MainActivity : AppCompatActivity() {
         bundle.putSerializable(FAVOURITES, favourites)
         outState.putBundle(FAVOURITES, bundle)
     }
-    
-    fun onInviteBtnClick(view: View?) {
-        if (view == null || view !is Button) return
-        val intent = Intent(ACTION_SEND)
-        intent.type = "text/plain"
-        val title = resources.getString(R.string.chooser)
-        val chooser = Intent.createChooser(intent, title)
-        intent.resolveActivity(packageManager)?.let {
-            startActivity(chooser)
-        }
-    }
 
     fun onFavouritesIconClick(view: View?) {
         if (view == null || view !is ImageView) return
         openFavouritesActivity(favourites, filmItems, FILM_FAVOURITES_REQUEST_CODE)
-    }
-
-    fun onModeSwitchClick(view: View?) {
-        if (view == null || view !is Switch) return
-        if (view.isChecked) {
-            setDefaultNightMode(MODE_NIGHT_YES)
-        } else {
-            setDefaultNightMode(MODE_NIGHT_NO)
-        }
-        recreate()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
