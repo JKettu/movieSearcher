f79709a403352b0abfaab24384d24d7e8ff50586
nataliya.lebedeva <nataliya.i.lebedeva@rt.ru>
2020-02-09 16:55:12 +0300

    add models for calling "the movie db"

---
+++ app/src/main/java/ru/kettu/moviesearcher/constants/QueryParamsConstants.kt
@@ -0,0 +1,26 @@
+package ru.kettu.moviesearcher.constants
+
+object QueryParamsConstants {
+    const val API_KEY = "api_key"
+    const val LANGUAGE = "language"
+
+    const val PAGE = "page"
+    const val TOTAL_RESULTS = "total_results"
+    const val TOTAL_PAGES = "total_pages"
+    const val RESULTS = "results"
+
+    const val POPULARITY = "popularity"
+    const val VOTE_COUNT = "vote_count"
+    const val VIDEO = "video"
+    const val POSTER_PATH = "poster_path"
+    const val ID = "id"
+    const val ADULT = "adult"
+    const val BACKDROP_PATH = "backdrop_path"
+    const val ORIGINAL_LANGUAGE = "original_language"
+    const val ORIGINAL_TITLE = "original_title"
+    const val GENRE_IDS = "genre_ids"
+    const val TITLE = "title"
+    const val VOTE_AVERAGE = "vote_average"
+    const val OVERVIEW = "overview"
+    const val RELEASE_DATE = "release_date"
+}
---
+++ app/src/main/java/ru/kettu/moviesearcher/models/network/FilmDetails.kt
@@ -0,0 +1,33 @@
+package ru.kettu.moviesearcher.models.network
+
+import com.google.gson.annotations.SerializedName
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.ADULT
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.BACKDROP_PATH
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.GENRE_IDS
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.ID
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.ORIGINAL_LANGUAGE
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.ORIGINAL_TITLE
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.OVERVIEW
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.POPULARITY
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.POSTER_PATH
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.RELEASE_DATE
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.TITLE
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.VIDEO
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.VOTE_AVERAGE
+import ru.kettu.moviesearcher.constants.QueryParamsConstants.VOTE_COUNT
+
+data class FilmDetails (@SerializedName(POPULARITY) var popularity: Double,
+                        @SerializedName(VOTE_COUNT) var voteCount: Int,
+                        @SerializedName(VIDEO) var video: Boolean,
+                        @SerializedName(POSTER_PATH) var posterPath : String,
+                        @SerializedName(ID) var id: Int,
+                        @SerializedName(ADULT) var adult: Boolean,
+                        @SerializedName(BACKDROP_PATH) var backdropPath: String,
+                        @SerializedName(ORIGINAL_LANGUAGE) var originalLang: String,
+                        @SerializedName(ORIGINAL_TITLE) var originalTitle: String,
+                        @SerializedName(GENRE_IDS) var genreIds: List<Int>,
+                        @SerializedName(TITLE) var title: String,
+                        @SerializedName(VOTE_AVERAGE) var voteAverage: Double,
+                        @SerializedName(OVERVIEW) var overview: String,
+                        @SerializedName(RELEASE_DATE) var releaseDate: String) {
+}
